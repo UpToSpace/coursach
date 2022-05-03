a0aa1bca46134c0adb68e86d5f9d434f6f853536
Lera <valerie1432mail.ru>
2022-05-03 23:37:13 +0300

    0305

---
+++ MyLove/Infrastructure/Commands/CloseApplicationCommand.cs
@@ -0,0 +1,22 @@
+﻿using MyLove.Infrastructure.Commands.Base;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MyLove.Infrastructure.Commands
+{
+    //class CloseApplicationCommand : Command
+    //{
+    //    //public override bool CanExecute(object parameter)
+    //    //{
+    //    //    return true;
+    //    //}
+
+    //    //public override void Execute(object parameter)
+    //    //{
+    //    //    throw new NotImplementedException();
+    //    //}
+    //}
+}
---
+++ MyLove/ViewModels/MainWindowViewModel.cs
@@ -0,0 +1,47 @@
+﻿using MyLove.Infrastructure.Commands;
+using MyLove.ViewModels.Base;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Input;
+
+namespace MyLove.ViewModels
+{
+    internal class MainWindowViewModel : ViewModel
+    {
+
+        public ICommand CloseApplicationCommand { get; }
+        private bool CanCloseApplicationCommandExecute(object o) => true;
+        private void OnCloseApplicationCommandExecuted(object o)
+        {
+
+        }
+
+        public ICommand ChangeThemeCommand { get; }
+        private void OnChangeThemeCommandExecuted(object o)
+        {
+            string theme = o as string;
+            switch (theme)
+            {
+                case "light":
+                    Application.Current.Resources.MergedDictionaries[1].Source = new Uri("./Views/Themes/LightTheme.xaml", UriKind.RelativeOrAbsolute);
+                    break;
+                case "dark":
+                    Application.Current.Resources.MergedDictionaries[1].Source = new Uri("./Views/Themes/DarkTheme.xaml", UriKind.RelativeOrAbsolute);
+                    break;
+                default:
+                    break;
+            }
+        }
+
+
+        public MainWindowViewModel()
+        {
+            CloseApplicationCommand = new RelayCommand(OnCloseApplicationCommandExecuted, CanCloseApplicationCommandExecute);
+            ChangeThemeCommand = new RelayCommand(OnChangeThemeCommandExecuted);
+        }
+    }
+}
